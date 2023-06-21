29c4d5ea3a8f8c6769f6444840ad0374e96b7eb0
saychelsea11 <31114603+saychelsea11@users.noreply.github.com>
2023-06-20 21:30:01 -0400

    Update real_estate_market_analysis_streamlit.py

--- Streamlit_App/real_estate_market_analysis_streamlit.py
+++ Streamlit_App/real_estate_market_analysis_streamlit.py
@@ -78,10 +78,8 @@ if st.button('Enter'):
     st.write("")
 
     historical_timeseries_grid(df_zillow_timeseries)
-    fig3 = plt.figure(figsize=(36,25))
     #uni_lineplot(df_zillow_timeseries_mean,'Timeline','Housing Price($)',f'Mean and Median Historical Housing Price in {state_choice}','Mean')
     #uni_lineplot(df_zillow_timeseries_median,'Timeline','Housing Price($)',f'Mean and Median Historical Housing Price in {state_choice}','Median')
-    st.pyplot(fig3)
     
     st.write("")
     st.write("")
